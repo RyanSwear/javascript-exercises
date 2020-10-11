a6733e31096d850197e816860f5df817024dfd73
Swear <ryanmswearingen@gmail.com>
2020-10-11 14:33:30 -0700

    completed repeatedString assignment

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(phrase, repetition) {
+    if (repetition < 0)
+    {
+        return 'ERROR';
+    }
     let answer ="";
     for (let index = 0; index < repetition; index++) {
         answer = answer + phrase;
