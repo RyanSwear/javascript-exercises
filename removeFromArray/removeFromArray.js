251e90ae8b9d89ff5db76c13ba75103aa95955c5
Swear <ryanmswearingen@gmail.com>
2020-10-11 16:09:51 -0700

    finished remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,11 +1,26 @@
-const removeFromArray = function(arr, item) {
+const removeFromArray = function(arr) {
+    let args = Array.prototype.slice.call(arguments, 1);
+    console.log("args:\n");
+    console.log(args);
+    for (let i = 0; i < args.length; i++)
+    {
+        const itemIndex = (element) => element === args[i];
+        let index = (arr.findIndex(itemIndex));
+        console.log("index:" + index);
+        if (index > -1)
+        {
+            let newArr = arr.splice(index, 1);
+            console.log(newArr);
+        }
+        
+    }
     
-    const itemIndex = (element) => element == item;
-    let index = (arr.findIndex(itemIndex));
-    console.log(index);
-    let newArr = arr.splice(index, 1);
-    console.log(newArr);
+    
+    console.log(arr);
+    console.log("\n");
     return arr;
+
+
 }
 
 module.exports = removeFromArray
