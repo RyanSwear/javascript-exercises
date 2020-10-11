4307c5a7fba325e8e716ea2bc1bb7848e542823c
Swear <ryanmswearingen@gmail.com>
2020-10-11 16:54:27 -0700

    completed sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(startN, endN) {
+    var answer = 'ERROR';
+    var sum = 0;
+    if (startN === NaN || endN === NaN)
+    {
+        return 'ERROR';
+    }
+    if (typeof startN === 'string' || typeof endN === 'string')
+    {
+        return 'ERROR';
+    }
+    if (startN > -1 && endN > -1)
+    {
+        if (startN > endN)
+        {
+            for (let index = endN; index <= startN; index++) {
+                sum = sum + index;
+            
+            }
+        }
+        else{
+            for (let index = startN; index <= endN; index++) {
+                sum = sum + index;
+            
+            }
+        }
+        answer = sum;
+    }   
+    
+    return answer;
 }
 
 module.exports = sumAll
