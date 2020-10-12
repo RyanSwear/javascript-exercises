4aa70f0c3947936865c157a957bed8f09f2e6562
Swear <ryanmswearingen@gmail.com>
2020-10-11 17:06:41 -0700

    completed first test leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let answer = 'ERROR';
+    let leapYear = false;
+    if (year > 0)
+    {
+        if(year % 4 == 0)
+        {
+            leapYear = true;
+        }
+    }
+    answer = leapYear;
+    return answer;
 }
 
 module.exports = leapYears
