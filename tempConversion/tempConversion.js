6201fb1bf1ecebb70f87cf7ee099f64a0a7e78be
Swear <ryanmswearingen@gmail.com>
2020-10-11 17:20:31 -0700

    completed tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(number) {
+  let answer = 0;
+  answer = (number - 32) * (5 / 9);
+  answer = Math.round(answer * 10) / 10;
+  return answer;
 }
 
-const ctof = function() {
-
+const ctof = function(number) {
+  let answer = 0;
+  answer = (number * (9 / 5)) + 32;
+  answer = Math.round(answer * 10) / 10;
+  return answer;
 }
 
 module.exports = {
