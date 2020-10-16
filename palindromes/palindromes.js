647e374ddcc103a74278e3cfa026633de3590509
Swear <ryanmswearingen@gmail.com>
2020-10-15 21:29:11 -0700

    completed palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function() {
+const palindromes = function(str) {
+
+    var newSen = str.match(/[^_\W]+/g).join('');
+    var sen = newSen.toUpperCase();
+    let i = newSen.length - 1;
+    let j = 0;
+    let palindrome = true;
+    while (palindrome && (j < sen.length / 2))
+    {
+        if (sen[i] != sen[j])
+        {
+            palindrome = false;
+        }
+        i--;
+        j++;
+    }
+    return palindrome;
 
 }
 
