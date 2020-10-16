be3619ab904a6925b9f79b4e4577274206d7a728
Swear <ryanmswearingen@gmail.com>
2020-10-15 21:06:21 -0700

    completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,43 @@
-function add () {
-	
+function add (number1, number2) {
+	return (number1 + number2);
 }
 
-function subtract () {
-	
+function subtract (number1, number2) {
+	return (number1 - number2);
 }
 
-function sum () {
-	
+function sum (numbers) {
+	let answer = 0;
+	for (let i = 0; i < numbers.length; i++)
+	{
+		answer = answer + numbers[i];
+	}
+	return answer;
 }
 
-function multiply () {
-	
+function multiply (numbers) {
+	let answer = 0;
+	for (let i = 0; i < numbers.length; i++)
+	{
+		if (i == 0){
+			answer = numbers[i];
+		}else{answer = answer * numbers[i];}
+		
+	}
+	return answer;
 }
 
-function power() {
-	
+function power(number1, number2) {
+	return(Math.pow(number1, number2));
 }
 
-function factorial() {
-	
+function factorial(number1) {
+	let answer = 1;
+	for (let i = 1; i <= number1; i++)
+	{
+		answer = answer * i;
+	}
+	return answer;
 }
 
 module.exports = {
